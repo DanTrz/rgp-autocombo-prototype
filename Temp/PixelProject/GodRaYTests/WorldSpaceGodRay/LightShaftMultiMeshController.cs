fe1a63e9552db69d19276774e3d2b7558e3deb0e
DanTrz <danielmtp@hotmail.com>
2025-07-01 10:14:30 +0100

    Updated MultiMesh Controller for Light Shaft to better handle Global vs Local Position

--- Temp/PixelProject/GodRaYTests/WorldSpaceGodRay/LightShaftMultiMeshController.cs
+++ Temp/PixelProject/GodRaYTests/WorldSpaceGodRay/LightShaftMultiMeshController.cs
@@ -24,13 +24,12 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 
 	[ExportGroup("Raycast Settings")]
 	[Export] private float _rayLenght { get; set; } = 45.0f;
-	[Export(PropertyHint.Layers3DRender)] public uint CollisionLayers { get; set; }
+	[Export(PropertyHint.Layers3DRender)] public uint RaycastCollisionLayers { get; set; } = 1;
+	[Export] private float _worldBoundMaxSize { get; set; } = 500.0f;
+	[Export] private bool _showDebugSpheres { get; set; } = false;
 
 	private Dictionary<int, InstanceCollider> _instanceList { get; set; } = new(); //int=IntanceID /--/ InstanceCollider=InstanaceInfo
 	private bool _hasCollidersMissing = true;
-	// private PhysicsRayQueryParameters3D _raycast;
-
-
 
 
 	public override void _Ready()
@@ -62,27 +61,25 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 		Multimesh.InstanceCount = _instanceCount;
 		Multimesh.VisibleInstanceCount = _instanceCount;
 
-		//DEBUG - TEST ONLY
-		CleanAllDebugSpheres();
-		//DEBUG - TEST ONLY
+		CleanAllDebugSpheres(); //DEBUG - TEST ONLY
 
 		for (int i = 0; i < Multimesh.InstanceCount; i++)
 		{
-			Vector3 newLocalPos = new Vector3((i * _position.X), _position.Y, _position.Z); //Seems an offset from parent (Local Pos)
+			Vector3 newLocalPos = new Vector3((i * _position.X), _position.Y, _position.Z); //LocalPos
 			Vector3 centerGlobalWorldPos = this.GlobalTransform * newLocalPos; //Convert to World Position (Global Position)
 
+			//Update Instance List for raycast logic
 			_instanceList[i] = new InstanceCollider(centerGlobalWorldPos, newLocalPos, false);
+			CreateDebugSphere(centerGlobalWorldPos, Colors.Green);// World position center 	//DEBUG - TEST ONLY
 
-			//DEBUG - TEST ONLY
-			CreateDebugCircle(centerGlobalWorldPos, Colors.Green);// World position center
 
+			//Setup transform and pass it to the MultiMesh for each instance
 			float newRotation = Mathf.DegToRad(_rotationZ);
 			Basis newBasis = new Basis(Vector3.One, newRotation); //Apply Rotation
 			newBasis.Column1 *= _scale.Y; //Scale just the Y axis
 			newBasis.Column2 *= _scale.Z; //Scale just the Z axis
 			newBasis.Column0 *= _scale.X; //Scale just the X axis //use basis.Column0 * RotationValue;
 
-			//Set position. SetInstanceTransform is relative to parent
 			Multimesh.SetInstanceTransform(i, new Transform3D(newBasis, newLocalPos));
 			Log.Debug($"Instance {i} set to GlobalPos {centerGlobalWorldPos}, LocalPos {newLocalPos}");
 		}
@@ -94,19 +91,20 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 		if (_instanceList.Count == 0) return; //TODO: In the future we can find a way to check if the "entire list" is already set customdata. 
 
 		MultiMesh Multimesh = this.Multimesh;
-		//DEBUG - TEST ONLY
-		//Define Vector3 Offset for shadow control. 
-		Vector3 minWorld = new Vector3(-500, -500, -500);
-		Vector3 maxWorld = new Vector3(500, 500, 500);
-		//DEBUG - TEST ONLY
+
+		//Define world bounds (used for packing/unpacking vectors to be sent to shader)
+		Vector3 minWorld = new Vector3(-_worldBoundMaxSize, -_worldBoundMaxSize, -_worldBoundMaxSize);
+		Vector3 maxWorld = new Vector3(_worldBoundMaxSize, _worldBoundMaxSize, _worldBoundMaxSize);
+
 
 		//Loop our InstanceList and Send the RayCast for Collision from their World Position Center
 		for (int i = 0; i < _instanceList.Count; i++)
 		{
-			if (_instanceList[i].HasCustomData) continue; //Skipt if we already passed CustomData to the Shader
+			if (_instanceList[i].PassedCustomData) continue; //Skipt if we already passed CustomData to the Shader
 
-			if (_instanceList[i].HasCollided)
+			if (_instanceList[i].HasCollided) //Have collision (we got collider pos) but have not yet passed it to Shader CustomData
 			{
+				//Normalize collider (required for the Shader to receive this as CustomData)
 				Vector3 colliderPos = _instanceList[i].ColliderPosition;
 				Vector3 normalizedColliderPos = new Vector3(
 					Mathf.InverseLerp(minWorld.X, maxWorld.X, colliderPos.X),
@@ -115,24 +113,21 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 				);
 
 				Multimesh.SetInstanceCustomData(i, new Color(normalizedColliderPos.X, normalizedColliderPos.Y, normalizedColliderPos.Z, 1));
-				CreateDebugCircle(colliderPos, Colors.Red);
-				_instanceList[i].HasCustomData = true;
+				CreateDebugSphere(colliderPos, Colors.Red);     //DEBUG - TEST ONLY
+				_instanceList[i].PassedCustomData = true;
 			}
 			else //Has not collided yet. Need to send a RayCast
 			{
 				//Get instance Global Position and send a RayCast to find collider
 				Vector3 centerGlobalWorldPos = _instanceList[i].GlobalPosition;
 				Vector3 centerLocalPos = _instanceList[i].LocalPosition;
-
-				//TODO - Wich one is more correct? Should we fire the Raycast from GlobalPos or is it influence by parent and local pos?
-				SendRaycast(i, centerGlobalWorldPos, new Vector3(0, -1, 0));
-				//SendRaycast(i, centerLocalPos, new Vector3(0, -1, 0));
+				SendRaycast(i, centerGlobalWorldPos, -this.Transform.Basis.Y);
 
 			}
 		}
 
-		//Check if all instances have collided and update class level
-		_hasCollidersMissing = _instanceList.Values.Any(instance => !instance.HasCustomData);
+		//Check if all instances have collided and update class level variable
+		_hasCollidersMissing = _instanceList.Values.Any(instance => !instance.PassedCustomData);
 	}
 
 	private void SendRaycast(int instanceIndex, Vector3 raycastStart, Vector3 _raycastDirection)
@@ -140,30 +135,22 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 		if (_instanceList[instanceIndex].HasCollided) return;
 
 		Vector3 colliderGlobalPos = Vector3.Zero;
-		//_raycastDirection = -this.GlobalTransform.Basis.Y;
 
+		//Create a Raycast and check if it hits anything
 		var spaceState = GetWorld3D().DirectSpaceState;
 		var raycastEndPoint = raycastStart + _raycastDirection * _rayLenght;
-
 		var query = PhysicsRayQueryParameters3D.Create(raycastStart, raycastEndPoint);
 		query.CollideWithAreas = false;
 		query.CollideWithBodies = true;
-		query.CollisionMask = CollisionLayers;
-
+		query.CollisionMask = RaycastCollisionLayers;
 		Godot.Collections.Dictionary result = spaceState.IntersectRay(query);
 
-		if (result.Count > 0) //while (result.Count < 0)
+		if (result.Count > 0) //If the Raycast hit something we get the collider and updated _instanceList
 		{
 			if (result.TryGetValue("collider", out var collider))
 			{
-
-				Node3D colliderNode = (Node3D)collider; // Cast the Variant to a Node
-														// Log.Debug($"Collider is: {colliderNode.Name}");
-
-				// Vector3 colliderLocalPos = result["position"].AsVector3();
-				// colliderGlobalPos = this.GlobalTransform * colliderLocalPos; //Actual World Position (Global Position)
-
-				colliderGlobalPos = result["position"].AsVector3(); // already global??
+				// Node3D colliderNode = (Node3D)collider; // Cast the Variant to a Node (Optional)
+				colliderGlobalPos = result["position"].AsVector3(); // This returns a GlobalPosition
 
 				_instanceList[instanceIndex].HasCollided = true;
 				_instanceList[instanceIndex].ColliderPosition = colliderGlobalPos;
@@ -178,8 +165,9 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 		}
 	}
 
-	private void CreateDebugCircle(Vector3 position, Color color)
+	private void CreateDebugSphere(Vector3 position, Color color)
 	{
+		if (!_showDebugSpheres) return;
 		var mesh = new SphereMesh();
 		mesh.Radius = 0.5f;
 		var material = new StandardMaterial3D();
@@ -213,7 +201,7 @@ public partial class LightShaftMultiMeshController : MultiMeshInstance3D
 
 		public Vector3 ColliderPosition { get; set; }
 
-		public bool HasCustomData { get; set; } = false;
+		public bool PassedCustomData { get; set; } = false;
 
 		public InstanceCollider(Vector3 globalPos, Vector3 localPos, bool hasCollided)
 		{
